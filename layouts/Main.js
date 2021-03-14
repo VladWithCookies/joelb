f22d8a2daf601a846d8ba312dd87ae868afaae34
Vlad V <summervibeswith9pm@gmail.com>
2021-03-14 17:06:21 +0200

    Added terms page

--- layouts/Main.js
+++ layouts/Main.js
@@ -13,9 +13,6 @@ const Main = ({ title, children }) => (
     <div className="flex flex-col h-screen">
       <Header />
       <main className="flex flex-col flex-grow bg-gray-100 pt-4 md:pt-14">
-        <h1 hidden>
-          Joel Baker Ministry Blog
-        </h1>
         {children}
       </main>
       <Footer />
